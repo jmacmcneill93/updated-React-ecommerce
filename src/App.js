a3eb40b1667c063dfe48ec680f541ee5367b8971
Josh McNeill <joshmcneill@macbook-pro.attlocal.net>
2020-07-08 17:50:27 -0400

    fixed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import GlobalFonts from './fonts/fonts';
-import Products from './components/ProductsPage/Products';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Contact from './components/Contact/Contact';
-import about from './components/AboutUs/about';
-import Navbar from './components/Navbar/Nav';
-import Footer from './components/Footer/Footer';
 import Container from '../src/components/container';
 import './App.css';
 
--- src/components/ProductsPage/Products.js
+++ src/components/ProductsPage/Products.js
@@ -3,7 +3,7 @@ import Navbar from '../Navbar/Nav';
 import Filter from '../Filter/Filter';
 import Footer from '../Footer/Footer';
 import './products.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 
  class Products extends React.Component {
     render() {
